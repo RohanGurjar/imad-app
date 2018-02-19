af9b54cd3dac703aa336abec1b58c0d292cd5d5e
Rohan Gurjar <rohan.gurjar467@gmail.com>
2018-02-19 20:33:37 +0100

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,98 +5,20 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articles = {
-    'article-one': {
-        title: 'Article One | Rohan Gurjar',
-        heading: 'Article One',
-        date: 'Aug 15, 2017',
-        content:
-        `<p>
-            This is the content for my first article.
-        </p>
-        <p>
-            This is the content for my first article.
-        </p>
-        <p>
-            This is the content for my first article.
-        </p>`
-    },
-    'article-two': {
-        title: 'Article Two | Rohan Gurjar',
-        heading: 'Article Two',
-        date: 'Aug 15, 2017',
-        content:
-        `<p>
-            This is the content for my second article.
-        </p>`
-    },
-    'article-three': {
-        title: 'Article Three | Rohan Gurjar',
-        heading: 'Article Three',
-        date: 'Aug 15, 2017',
-        content:
-        `<p>
-            This is the content for my third article.
-        </p>`
-    }
-};
-
-function createTemplate(data) {
-    var title = data.title;
-    var date = data.date;
-    var heading = data.heading;
-    var content = data.content;
-    var htmlTemplate = `
-        <html>
-            <head>
-                <title>
-                    ${title}
-                </title>
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <link href="/ui/style.css" rel="stylesheet" />
-            </head>
-            <body>
-                <div class="container">
-                    <div>
-                        <a href="/">Home</a>
-                    </div>
-                    <hr/>
-                    <h3>
-                        ${heading}
-                    </h3>
-                    <div>
-                        ${date}
-                    </div>
-                    <div>
-                        ${content}
-                    </div>
-                </div>
-            </body>
-        </html>
-        `;
-    return htmlTemplate;
-}
-
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var counter = 0;
-app.get('/counter', function(req, res) {
-    counter = counter + 1;
-    res.send(counter.toString());
+app.get('/article-one', function(req, res) {
+    
 });
 
-var names = [];
-app.get('/submit-name', function (req, res) {
-  var name = req.query.name;
-  names.push(name);
-  res.send(JSON.stringify(names));
+app.get('/article-two', function(req, res) {
+    
 });
 
-app.get('/:articleName', function (req, res) {
-    var articleName = req.params.articleName;
-    res.send(createTemplate(articles[articleName]));
+app.get('/article-three', function(req, res) {
+    
 });
 
 app.get('/ui/style.css', function (req, res) {
@@ -105,10 +27,6 @@ app.get('/ui/style.css', function (req, res) {
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
-app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
 // Do not change port, otherwise your app won't run on IMAD servers
